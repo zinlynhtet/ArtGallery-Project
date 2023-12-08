bc8e9cb9a8efde7be4f62afbc59e7aac487220a6
MacFlick <mackk5504@gmail.com>
2023-12-09 01:25:41 +0630

    Art-Gallery Project

---
+++ js/script.js
@@ -0,0 +1,107 @@
+// eslint-disable-next-line camelcase
+const Tbl_Art = [
+  {
+    ArtId: 1,
+    owner: 'Peter',
+    image: 'images/1.jpg',
+    ArtName: 'Marina',
+    ArtDescription: 'သူငယ်ချင်း အတွက် အမှတ်တရ ဆွဲပေးထားတာပါ',
+  },
+  {
+    ArtId: 2,
+    owner: 'Olivia',
+    image: 'images/2.jpg',
+    ArtName: 'Kudae',
+    ArtDescription: 'သူငယ်ချင်း အတွက် အမှတ်တရ ဆွဲပေးထားတာပါ',
+  },
+  {
+    ArtId: 3,
+    owner: 'Liam',
+    image: 'images/3.jpg',
+    ArtName: 'Kudae1',
+    ArtDescription: 'သူငယ်ချင်း အတွက် အမှတ်တရ ဆွဲပေးထားတာပါ',
+  },
+  {
+    ArtId: 4,
+    owner: 'Sophia',
+    image: 'images/4.jpg',
+    ArtName: 'Kudae2',
+    ArtDescription: 'သူငယ်ချင်း အတွက် အမှတ်တရ ဆွဲပေးထားတာပါ',
+  },
+  {
+    ArtId: 5,
+    owner: 'Noah',
+    image: 'images/5.jpg',
+    ArtName: '3',
+    ArtDescription: 'သူငယ်ချင်း အတွက် အမှတ်တရ ဆွဲပေးထားတာပါ',
+  },
+  {
+    ArtId: 6,
+    owner: 'Peter',
+    image: 'images/6.jpg',
+    ArtName: 'Tick Tick Boom',
+    ArtDescription: 'အာ့ဇာတ်ကားကြည့်ပီး Andrew Garfield ရဲ့ ဘဝလောကဓံကို ပီပြင်အောင်ရိုက်ထားဝာာ သဘောကျပီး',
+  },
+  {
+    ArtId: 7,
+    owner: 'Ava',
+    image: 'images/7.jpg',
+    ArtName: 'Not Only Doodle',
+    ArtDescription: 'nဒီပန်းချီကားကို Doodle ပဲမဟုတ်ဘူး ဆိုတဲ့ခေါင်းစဉ်လေးပေးထားရတဲ့အကြောင်းကတော့ Doodle Art နဲ့ပတ်သတ်ပြီး တစ်ချို့သောသူတွေ အမြင်မှာ Doodle ဟာအခြေခံတွေ မလိုဘူး ပုံဆွဲတတ်ဖို့မလိုဘူး၊ ပုံမဆွဲတတ် ပန်းချီအခြေခံတွေမတတ်လည်း Doodle Art ကိုဖန်တီးလို့ရတယ်ဆိုပေမဲ့ Doodle Art ဆိုတာတကယ်ရှာဖွေနိုင်ရင် အလွန်အင်မတန်မှ စိတ်ဝင်စားစရာကောင်းတဲ့ Art style တစ်မျိုးဖြစ်ပါတယ်။ ရှာဖွေလေတွေ့ရှိလေ၊ လေ့လာလေ နက်နဲလေဆိုတဲ့ အနုပညာတစ်မျိုးပါပဲ။ ပန်းချီကို ပုံစံမျိုးစုံနဲ့ ရှုမြင်လက်ခံကြတဲ့အထဲမှာမှ\nDoodle ဆွဲရင်ပန်းချီဆရာမဟုတ်ဘူး Doodle ပုံ က ပန်းချီကားမဖြစ်နိုင်ဘူး Realism တွေဆွဲမှ ပန်းချီကားဖြစ်တယ်လို့သတ်မှတ်တတ်တဲ့သူများလဲရှိပြီးတော့ Realism တွေကြီးပဲဆွဲရင်လဲ ဓာတ်ပုံကိုပဲကြည့်ပြီးဆွဲတတ်တယ် ကိုယ်ပိုင်ဖန်တီးမှုမရှိဘူး တွေးခေါ်မှုမရှိဘူးဆိုပြီး သတ်မှတ်တတ်သောသူများလဲရှိပါသည်။ ထို့ကြောင့် Doodle Art နဲ့အတူ Realism ဆန်တဲ့ Still life နဲ့ Object တွေကိုပါ ပန်းချီကားထဲတွင် ထည့်သွင်းရေးချယ်ဖို့ ဆုံးဖြတ်ပြီး ဒီပန်းချီကားကို ဖန်တီးဖြစ်ခဲ့ပါတယ်ခင်ဗျာ။',
+  },
+  {
+    ArtId: 8,
+    owner: 'Jackson',
+    image: 'images/8.jpg',
+    ArtName: 'Break Up',
+    ArtDescription: 'သေကွဲရယ် ၊ ရှင်ကွဲရယ် တစ်နည်းနည်းသောကွဲခြင်းနဲ့ ဆက်ဆံရေးတွေဟာ မတော်တဆကျောခိုင်းရပ်မိကြလိမ့်ဦးမယ်။\nမတည်မြဲခြင်းကြားမှာ ငါတို့ဟာ ပျော်ခဲ့ဖူးမယ် ၊ ငိုခဲ့ဖူးမယ် ၊ လွမ်းခဲ့ဖူးမယ် ၊ ဒေါသတွေလည်းထွက်ခဲ့ဖူးကြမယ်။ \nသမုဒယသစ္စာကိုလက်ကိုင်ထားရင်း နှင်း‌ဆီတွေကြွေတာကိုလည်း ငါတို့တွေ ကြည်ကြည်နူးနူးကြည့်ဖူးကြလိမ့်မယ်။\nဘယ်လိုခံစားပြီးဆွဲထားလဲဆို‌တော့ သီချင်းစာသားတစ်ကြောင်းကို ကြားပြီးတဲ့နောက်မှာ ဒီလိုလေးဆွဲဖို့ idea ရခဲ့ပါတယ်။  ပန်းချီကားရဲ့ အဓိပ္ပာယ်ကတော့ မပြောတော့ဘူးနော်။ ဒီပန်းချီကားကိုကြည့်ရင်းနဲ့ ကြည့်သူမှာပေါ်လာတဲ့ ခံစားချက် ၊ ကြည့်သူ သဘောပေါက်သွားတဲ့ အကြောင်းအရာဟာ သူ့ရဲ့ အဓိပ္ပာယ်ပါပဲ။',
+  },
+  {
+    ArtId: 9,
+    owner: 'Isabella',
+    image: 'images/9.jpg',
+    ArtName: 'ဘုံကဖီး',
+    ArtDescription: 'ပုံကတော့အသိ ကဗျာဆရာလဲဟုတ် ပန်းချီဆရာလဲဖြစ်တဲ့ ကိုအီလစ်တို့ရဲ့ဘုံကဖီးလေးပါပဲ\nပန်းချီကားလေးအဓိပ္ပာယ်ကတော့ တစ်ပတ်မှာ36နာရီလက်ဖက်ရည်ဆိုင်ထိုင်ခဲ့တယ် \nတစ်ဖြည်းဖြည်းနဲ့ လက်ဖက်ရည်ဆိုင်ကခုံတွေလိုတို့ဘဝတွေပုဝင်နေခဲ့ ဘယ်လိုမှမေ့ပျောက်လို့မရတဲ့နေ့တွေ တစ်ဟုန်းထိုးမောင်းထွက်သွားတဲ့ဘဝတွေ \nတစ်ဘဝလုံးပုံအောခဲ့တဲ့ကစားဝိုင်း ငါတို့အားလုံးရဲ့ ဘုံကဖီးပါပဲ။',
+  },
+  {
+    ArtId: 10,
+    owner: 'Lucas',
+    image: 'images/10.jpg',
+    ArtName: 'ကနုဒ',
+    ArtDescription: 'ကြာပန်းတို့၏ဟန်ကိုယူ၍ကွေး၊ကော့၊ခွေ၊လိပ် ကာအလှအပတန်ဆာဆင်ထားသောကနုတ်ပန်းများနှင့်ကြာပန်း၏အလှတရားကိုခံစားသက်ဝင်ရင်း...',
+  },
+  {
+    ArtId: 11,
+    owner: 'Mia',
+    image: 'images/11.jpg',
+    ArtName: 'rose',
+    ArtDescription: 'သီချင်းဆိုရတာကြိုက်တာမို့ပါ။ ပန်းကလဲနှင်းဆီပန်းကြိုက်လို့ပါ။',
+  },
+  {
+    ArtId: 12,
+    owner: 'Aiden',
+    image: 'images/12.jpg',
+    ArtName: 'midnight dream',
+    ArtDescription: 'About the dream of young people \ncovid နဲ့ politics အခြေနေတွေခက်ခက်ခဲခဲဖြတ်ကျော်ခဲ့ရတဲ့လူငယ်တွေအတွက်ပါ။',
+  },
+];
+
+// eslint-disable-next-line no-unused-vars
+function flipCard(button) {
+  const card = button.closest('.over-card');
+  card.classList.toggle('flip');
+}
+const artGrid = document.getElementById('artGrid');
+
+const template = document.getElementById('artCardTemplate');
+
+Tbl_Art.forEach((artwork) => {
+  const cardClone = template.content.cloneNode(true);
+  cardClone.querySelector('img').src = artwork.image;
+  cardClone.querySelector('img').alt = artwork.ArtName;
+  cardClone.querySelector('h3').textContent = artwork.ArtName;
+  cardClone.querySelector('footer').textContent += artwork.owner;
+  cardClone.querySelector('.art-id').textContent = artwork.ArtId;
+  cardClone.querySelector('.art-description').textContent = artwork.ArtDescription;
+  artGrid.appendChild(cardClone);
+});
